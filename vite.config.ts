930bfd7de7804be3251e59af28060394203d4c09
455886774@qq.com <lu123456>
2023-01-05 15:10:15 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -34,4 +34,35 @@ export default defineConfig({
     }),
     ElementPlus(),
   ],
+  build: {
+    //打包文件大小超出警告-1500kb, 默认500kb
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }, // 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+        entryFileNames: "js/[name].[hash].js",
+        // 用于输出静态资源的命名，[ext]表示文件扩展名
+        assetFileNames: "[ext]/[name].[hash].[ext]",
+        // 用于命名代码拆分时创建的共享块的输出命名
+        // chunkFileNames: "js/[name].[hash].js",
+        // 拆分js到模块文件夹
+        chunkFileNames: (chunkInfo) => {
+          const facadeModuleId = chunkInfo.facadeModuleId
+            ? chunkInfo.facadeModuleId.split("/")
+            : [];
+          const fileName =
+            facadeModuleId[facadeModuleId.length - 2] || "[name]";
+          return `js/${fileName}/[name].[hash].js`;
+        },
+      },
+    },
+  },
 });
